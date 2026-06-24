4031055fc67787d33e574f41de2b73e54f438dce
neribautista <nerissabautistaa@gmail.com>
2026-06-23 20:39:46 -0500

    update for backend deployment with proper urls

--- backend/server.js
+++ backend/server.js
@@ -11,6 +11,7 @@ import cookieParser from 'cookie-parser';
 import { fileURLToPath } from 'url';
 
 dotenv.config();
+connectDB();
 
 const app = express();
 app.use(express.json());
@@ -18,15 +19,17 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 const corsOptions = {
-    origin: "http://localhost:5173", 
+    origin: process.env.CLIENT_URL || process.env.LOCAL_URL,
     credentials: true, 
 };
-
 app.use(cors(corsOptions));
 
 // Define __dirname for ES modules
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+
+// Serve the uploads folder as static
+app.use('/uploads', express.static(path.join(__dirname, '../uploads')));
 
 const PORT = process.env.PORT || 3001;
 
@@ -36,15 +39,12 @@ app.use("/api/contact", contactRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/orders", orderRoutes);
 
-// Serve the uploads folder as static
-app.use('/uploads', express.static(path.resolve(__dirname, '../uploads'))); 
+// Global error handling middleware
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).json({ message: "Something went wrong!" });
+});
 
-app.listen(PORT, async () => {
-    try {
-        await connectDB();
-        console.log(`Server running on port ${PORT}`);
-    } catch (error) {
-        console.error('Error connecting to the database:', error.message);
-        process.exit(1); // Exit the process with failure
-    }
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
 });
